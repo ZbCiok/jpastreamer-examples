ee96e7be3a84c366744596c62ea53ccdcf002db4
poltman <poltman@onet.pl>
2023-08-30 17:46:17 +0200

    logging 01

--- src/main/java/zjc/examples/spring/jpastreamer/repository/OrganizationRepository.java
+++ src/main/java/zjc/examples/spring/jpastreamer/repository/OrganizationRepository.java
@@ -1,11 +1,16 @@
 package zjc.examples.spring.jpastreamer.repository;
 
+import com.speedment.common.codegen.model.Interface;
 import com.speedment.jpastreamer.application.JPAStreamer;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import org.hibernate.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import zjc.examples.spring.jpastreamer.dto.OrganizationDTO;
+import zjc.examples.spring.jpastreamer.log.LogController;
 import zjc.examples.spring.jpastreamer.model.Organization;
 import zjc.examples.spring.jpastreamer.model.Organization$;
 
@@ -15,11 +20,13 @@ import java.util.stream.Collectors;
 @Repository
 public class OrganizationRepository {
 
-    private final JPAStreamer streamer;
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogController.class);
+    private final JPAStreamer  streamer;
 
     public OrganizationRepository(JPAStreamer streamer) { this.streamer = streamer; }
 
     public List<OrganizationDTO> findAll() {
+        LOGGER.info("OrganizationRepository::findAll");
         return streamer.stream(Organization.class)
                 .sorted(Organization$.name)
                 .map(OrganizationDTO::new)
@@ -53,4 +60,31 @@ public class OrganizationRepository {
         em.close();
         emf.close();
     }
+
+    interface DoInJPA {
+        public void doInJPA();
+    }
+
+//    public void save() {
+//        final EntityManagerFactory emf = Persistence.createEntityManagerFactory("testdb");
+//        final EntityManager em = emf.createEntityManager();
+//
+//        Organization organization = DoInJPA(entityManager -> {
+//            Organization org = new Organization()
+//                    .setName("High-Performance Java Persistence")
+//                    .setAuthor("Vlad Mihalcea");
+//
+//            Session session = em.unwrap(Session.class);
+//            session.persist(org);
+//            session.close();
+//
+//            LOGGER.info(
+//                    "Saving the Book entity with the id: {}",
+//                    id
+//            );
+//        });
+//
+//        em.close();
+//        emf.close();
+//    }
 }
